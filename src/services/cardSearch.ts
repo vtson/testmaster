cdb1772310b7cdc27f5709dcfe33f6370edc4662
vtson <vts.vuthanhson@gmail.com>
2026-03-02 10:09:03 +0700

    static image

--- src/services/cardSearch.ts
+++
@@ -1,8 +0,0 @@
-// src/services/cardSearch.ts
-import { } from '@/lib/master-strike-data/dist'
-
-const searchEngine = new CardSearchEngine()
-
-export async function searchCards(query: string) {
-  return searchEngine.search(query)
-}
